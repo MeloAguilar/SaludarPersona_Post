fa60630f9e4a0e8717318badbb752cc214bbf2aa
Melo Aguilar <carmelo.aguilar@iesnervion.es>
2021-11-02 12:24:19 +0100

    Agregar archivos de proyecto.

---
+++ SaludarPersona_Post_Entities/clsPersona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SaludarPersona_Post_Entities
+{
+    public class clsPersona
+    {
+        private string nombre;
+        private string apellidos;
+
+        public string Nombre{ get; set; }
+        public string Apellidos { get; set; }
+
+        public clsPersona(string _nombre, string _apellidos)
+        {
+            this.Nombre = _nombre;
+            this.Apellidos = _apellidos;
+        }
+    }
+}
---
+++ SaludarPersona_Post_UI/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SaludarPersona_Post_UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using SaludarPersona_Post_Entities;
+
+namespace SaludarPersona_Post_UI.Controllers
+{
+    public class HomeController : Controller
+    {
+ 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string _nombre, string _apellidos)
+        {
+            ViewBag.Nombre = _nombre;
+            ViewBag.Apellidos = _apellidos;
+            return View("Saludar");
+        }
+
+    }
+}
